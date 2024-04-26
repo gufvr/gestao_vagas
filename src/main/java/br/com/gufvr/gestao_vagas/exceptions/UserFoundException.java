a1272b70e460094df6b4e0fe60eed23d35819b22
GuFvr <gustavo.vfavero@gmail.com>
2024-04-25 22:34:00 -0300

    feat: Implement Use Case to handle existing user
    Created CreateCandidateUseCase to handle the scenario where a user with the same username or email already exists. If found, it throws a UserFoundException. Updated CandidateController to handle the exception and return a friendly error message with a bad request status code.

--- src/main/java/br/com/gufvr/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/gufvr/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package br.com.gufvr.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException {
   public UserFoundException() {
-    super("Uusário já existe");
+    super("Usuário já existe");
   }
 }
