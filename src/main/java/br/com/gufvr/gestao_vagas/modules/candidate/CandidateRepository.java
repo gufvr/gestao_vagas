e39482dc9085dc3eb99c6a76c56aed11db0c1c70
GuFvr <gustavo.vfavero@gmail.com>
2024-04-25 21:55:25 -0300

    feat: Implement CandidateRepository and create candidate in CandidateController
    Implemented CandidateRepository interface extending JpaRepository for CandidateEntity with UUID as the primary key. Injected CandidateRepository into CandidateController and implemented a POST endpoint to create a candidate using the repository's save method.

---
+++ src/main/java/br/com/gufvr/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,11 @@
+package br.com.gufvr.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+
+  
+}
