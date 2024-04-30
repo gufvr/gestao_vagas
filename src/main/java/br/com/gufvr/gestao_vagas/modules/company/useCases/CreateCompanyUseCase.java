3b2d08edd4085692a04b9963a6b81072ebcf6d3e
GuFvr <gustavo.vfavero@gmail.com>
2024-04-29 22:06:42 -0300

    feat: Implement CreateCompanyUseCase
    Added CreateCompanyUseCase to handle the creation of a company. Implemented logic to check if a company with the same username or email already exists. If found, throw a UserFoundException. Otherwise, save the new company entity using CompanyRepository.

---
+++ src/main/java/br/com/gufvr/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,25 @@
+package br.com.gufvr.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gufvr.gestao_vagas.exceptions.UserFoundException;
+import br.com.gufvr.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.gufvr.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class CreateCompanyUseCase {
+
+  @Autowired
+  private CompanyRepository companyRepository;
+  
+  public CompanyEntity execute(CompanyEntity companyEntity) {
+
+    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail()).ifPresent((user) -> {
+      throw new UserFoundException();
+    });;
+    
+    return this.companyRepository.save(companyEntity);
+  }
+
+}
