f50432e636924800dfde3e9831ce1a6b7e6894a2
GuFvr <gustavo.vfavero@gmail.com>
2024-05-02 22:53:17 -0300

    feat: Add Java JWT dependency and implement Company authentication
    Added Java JWT dependency for token authentication. Implemented AuthCompanyUseCase to authenticate company users. Added AuthCompanyDTO for authentication credentials. Implemented findByUsername method in CompanyRepository to retrieve company by username. Used PasswordEncoder to encode and verify passwords securely.

--- src/main/java/br/com/gufvr/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/br/com/gufvr/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gufvr.gestao_vagas.modules.company.entities.CompanyEntity;
 
+
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
   Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+  Optional<CompanyEntity> findByUsername(String username);
 }
